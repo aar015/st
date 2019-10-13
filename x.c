a9e563e3b1b94376e4d889e579bf57596cdda906
Avi <avirlincoln@gmail.com>
2019-10-13 11:29:47 -0500

    Clipboard Patch

--- x.c
+++ x.c
@@ -643,6 +643,8 @@ setsel(char *str, Time t)
 	XSetSelectionOwner(xw.dpy, XA_PRIMARY, xw.win, t);
 	if (XGetSelectionOwner(xw.dpy, XA_PRIMARY) != xw.win)
 		selclear();
+
+	clipcopy(NULL);
 }
 
 void
@@ -659,8 +661,8 @@ brelease(XEvent *e)
 		return;
 	}
 
-	if (e->xbutton.button == Button2)
-		selpaste(NULL);
+	if (e->xbutton.button == Button3)
+		clippaste(NULL);
 	else if (e->xbutton.button == Button1)
 		mousesel(e, 1);
 }
